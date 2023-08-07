371c9d4a86eb448ccc457044a56a72f28ae38e7f
DESKTOP-B7J0UDP\Guillaume <guillaume.d.degrandpre@gmail.com>
2023-08-07 09:45:19 -0400

    Run grading.py after downloading files

--- grading.sh
+++ grading.sh
@@ -2,3 +2,5 @@ wget --header 'Authorization: token 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' -O
 wget --header 'Authorization: token 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' -O grading_utils.py https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/grading_utils.py
 wget --header 'Authorization: token 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' -O target.json https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/target.json
 wget --header 'Authorization: token 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' -O .grading.env https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/.grading.env
+
+python grading.py
