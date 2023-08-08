8b09543f8bcd83ff8497dc2ab883ac5ebd53e142
DESKTOP-B7J0UDP\Guillaume <guillaume.d.degrandpre@gmail.com>
2023-08-08 13:22:52 -0400

    Rename .grading.env to .env

--- grading.sh
+++ grading.sh
@@ -1,7 +1,7 @@
 wget --header 'Authorization: token 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' -O grading.py https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/grading.py
 wget --header 'Authorization: token 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' -O grading_utils.py https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/grading_utils.py
 wget --header 'Authorization: token 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' -O target.json https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/target.json
-wget --header 'Authorization: token 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' -O .grading.env https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/.grading.env
+wget --header 'Authorization: token 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' -O .env https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/.env
 
 pip install -r requirements.txt
 python grading.py
