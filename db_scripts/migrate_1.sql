a2fb04064ed0851004353223e36ed09577ce0c5a
DESKTOP-B7J0UDP\Guillaume <guillaume.d.degrandpre@gmail.com>
2023-08-09 13:57:54 -0400

    Remove all enum types and replace with varchar

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,7 +1,8 @@
 ALTER TABLE singer RENAME TO musician;
 ALTER TABLE musician RENAME COLUMN singerName TO musicianName;
-ALTER TABLE musician ADD COLUMN (role ENUM("vocals", "guitar", "bass", "percussion", "violin", "piano", "other"));
+ALTER TABLE musician ADD COLUMN (role varchar(50));
 ALTER TABLE musician ADD COLUMN (bandName varchar(50));
+
 UPDATE musician SET role = "vocals", bandName = "Crazy Duo" WHERE musicianName = "Alina";
 UPDATE musician SET role = "piano", bandName = "Luna" WHERE musicianName = "Luna";
 UPDATE musician SET role = "guitar", bandName = "Mysterio" WHERE musicianName = "Mysterio";
@@ -9,7 +10,7 @@ UPDATE musician SET role = "percussion", bandName = "Crazy Duo" WHERE musicianNa
 
 CREATE TABLE band ( bandName varchar(50),
                     creation YEAR,
-                    genre ENUM("rock", "rap", "pop", "classical", "jazz"),
+                    genre varchar(50),
                     PRIMARY KEY (bandName));
 
 INSERT INTO band VALUES ("Crazy Duo", 2015, "rock"), ("Luna", 2009, "classical"), ("Mysterio", 2019 ,"pop");
