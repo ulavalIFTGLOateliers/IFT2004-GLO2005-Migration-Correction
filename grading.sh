8c289393ad67b1c235faa9d44145e6b8b15440fb
Guillaume Desmarais-de Grandpre <gdesmarais@biotwin.ai>
2024-03-01 08:32:01 -0500

    Change tokens

--- grading.sh
+++ grading.sh
@@ -1,7 +1,7 @@
-wget --header 'Authorization: token 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' -O grading.py https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/grading.py
-wget --header 'Authorization: token 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' -O grading_utils.py https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/grading_utils.py
-wget --header 'Authorization: token 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' -O target.json https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/grading/target.json
-wget --header 'Authorization: token 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' -O .env https://raw.githubusercontent.com/ulaval-atelier-bd/ulaval-atelier-bd-correction/master/.env
+wget --header 'Authorization: token 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' -O grading.py https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/grading.py
+wget --header 'Authorization: token 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' -O grading_utils.py https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/grading_utils.py
+wget --header 'Authorization: token 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' -O target.json https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/target.json
+wget --header 'Authorization: token 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' -O .env https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/.env
 
 pip install -r requirements.txt
 python grading.py
