8ca25839e83d5fa3a664ab39c3cb4d0a735bfef2
Guillaume Desmarais-de Grandpre <gdesmarais@biotwin.ai>
2024-03-02 19:40:11 -0500

    change token

--- grading.sh
+++ grading.sh
@@ -1,7 +1,7 @@
-wget --header 'Authorization: token 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' -O grading.py https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/grading.py
-wget --header 'Authorization: token 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' -O grading_utils.py https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/grading_utils.py
-wget --header 'Authorization: token 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' -O target.json https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/target.json
-wget --header 'Authorization: token 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' -O .env https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/.env
+wget --header 'Authorization: token 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' -O grading.py https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/grading.py
+wget --header 'Authorization: token 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' -O grading_utils.py https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/grading_utils.py
+wget --header 'Authorization: token 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' -O target.json https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/grading/target.json
+wget --header 'Authorization: token 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' -O .env https://raw.githubusercontent.com/ulavalIFTGLOateliers/IFT2004-GLO2005-Migration-Correction/master/.env
 
 pip install -r requirements.txt
 python grading.py
